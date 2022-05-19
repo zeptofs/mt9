6ad1f1984436e418de0979a0b648fe6b4dd13444
Zachariah Ingle <zach@zepto.com.au>
2022-05-20 09:30:23 +1000

    Move header record contract into seperate file

--- lib/mt9/validators.rb
+++ lib/mt9/validators.rb
@@ -2,23 +2,4 @@
 
 require "dry-validation"
 
-module MT9
-  module Validators
-    class HeaderRecordContract < Dry::Validation::Contract
-      DIRECT_CREDIT = 12
-      DIRECT_DEBIT = 20
-      FILE_TYPES = [DIRECT_CREDIT, DIRECT_DEBIT].freeze
-
-      schema do
-        required(:file_type).filled(:integer, included_in?: FILE_TYPES)
-        required(:account_number).filled(:string)
-        required(:due_date).filled(:date)
-        optional(:client_short_name).value(:string, size?: 0..20)
-      end
-
-      rule(:account_number) do
-        key.failure("must be 15 numeric characters") unless /^\d{15}$/.match(value)
-      end
-    end
-  end
-end
+require_relative "validators/header_record_contract"
