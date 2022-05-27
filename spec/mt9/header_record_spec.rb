6f6ebed73929be8975b167e9658e13895dce2e62
Zachariah Ingle <zach@zepto.com.au>
2022-05-27 16:07:20 +1000

    Update header record spec to use context blocks

--- spec/mt9/header_record_spec.rb
+++ spec/mt9/header_record_spec.rb
@@ -1,33 +1,54 @@
 # frozen_string_literal: true
 
 RSpec.describe MT9::HeaderRecord do
-  let(:header_record) do
+  subject(:header_record) { described_class.new(**header_record_args) }
+
+  let(:header_record_args) do
     {
       file_type: "12",
-      account_number: "123456789012345",
-      due_date: Date.parse("25/12/2020"),
-      client_short_name: "ACME Pty Ltd",
+      account_number: account_number,
+      due_date: due_date,
+      client_short_name: client_short_name,
     }
   end
 
-  describe "when generating header record" do
-    subject(:result) { described_class.new(**header_record).generate }
+  let(:account_number) { "123456789012345" }
+  let(:due_date) { Date.parse("25/12/2020") }
+  let(:client_short_name) { "ACME Pty Ltd" }
+
+  describe "#generate" do
+    subject(:result) { header_record.generate }
 
     it "creates correctly formatted string" do
-      expect(result).to eq("12123456789012345 20201225     ACME Pty Ltd                                                                                                                     \r\n") # rubocop:disable Layout/LineLength
+      expect(result).to eq("12123456789012345 20201225     ACME Pty Ltd                                               "\
+        "                                                                      \r\n")
     end
 
-    it "creates correctly formatted string with a long account number" do
-      header_record[:account_number] = "1234567890123456"
+    context "with a long account number" do
+      let(:account_number) { "1234567890123456" }
 
-      expect(result).to eq("12123456789012345620201225     ACME Pty Ltd                                                                                                                     \r\n") # rubocop:disable Layout/LineLength
+      it "creates correctly formatted string with a long account number" do
+        expect(result).to eq("12123456789012345620201225     ACME Pty Ltd                                             "\
+          "                                                                        \r\n")
+      end
     end
-  end
 
-  it "raises ValidationError with an incorrect header record" do
-    header_record[:file_type] = "999"
+    context "with no client short name" do
+      let(:client_short_name) { "" }
 
-    expect { described_class.new(**header_record) }.to \
-      raise_error(MT9::ValidationError, "Validation failed: file_type must be one of: 12, 20")
+      it "creates correctly formatted string" do
+        expect(result).to eq("12123456789012345 20201225                                                              "\
+          "                                                                        \r\n")
+      end
+    end
+
+    context "with invalid header record args" do
+      let(:account_number) { "1231231243123" }
+
+      it "raises ValidationError" do
+        expect { result }.to \
+          raise_error(MT9::ValidationError, "Validation failed: account_number must be 15 or 16 numeric characters")
+      end
+    end
   end
 end
